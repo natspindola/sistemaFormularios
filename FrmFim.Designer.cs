856b247c8e8b6566bf2d282eb0d3f8ceae0f0f39
natspindola <natspindola@hotmail.com>
2022-04-27 17:17:57 -0300

    layout finalização

--- FrmFim.Designer.cs
+++ FrmFim.Designer.cs
@@ -29,19 +29,85 @@ namespace sistemaFormularios
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnVendas = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(28, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Venda cadastrada com sucesso!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(344, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Favor, encaminhar o cliente para efetuar pagamento.";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(53, 149);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnInicio.TabIndex = 2;
+            this.btnInicio.Text = "Início";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            // 
+            // btnVendas
+            // 
+            this.btnVendas.Location = new System.Drawing.Point(186, 149);
+            this.btnVendas.Name = "btnVendas";
+            this.btnVendas.Size = new System.Drawing.Size(75, 23);
+            this.btnVendas.TabIndex = 3;
+            this.btnVendas.Text = "Vendas";
+            this.btnVendas.UseVisualStyleBackColor = true;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(319, 149);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
             // 
             // FrmFim
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 422);
+            this.ClientSize = new System.Drawing.Size(443, 214);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnVendas);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FrmFim";
-            this.Text = "FrmFim";
+            this.Text = "Finalização de venda";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnVendas;
+        private System.Windows.Forms.Button btnFechar;
     }
 }
